4bd80c9a0ff9a372ed797224ad1db4d9cbe16185
Elio <neto.elio@hotmail.com>
2024-06-02 19:03:29 -0300

    Alteração na função listar vendas

--- vendas.py
+++ vendas.py
@@ -23,7 +23,15 @@ def inserir_carro_venda(id_venda, id_carro, preco_venda):
 def listar_vendas():
     with sqlite3.connect('main.sqlite') as conexao:
         cursor = conexao.cursor()
-        cursor.execute('SELECT * FROM vendas')
+        cursor.execute('''SELECT vendas_id, id_cliente, data_venda FROM vendas''')
         vendas = cursor.fetchall()
-        for vendas in vendas:
-          print(vendas)
+        for venda in vendas:
+            print(f"ID da venda: {venda[0]}")
+            print(f"ID do cliente: {venda[1]}")
+            print(f"Data da venda: {venda[2]}")
+        cursor.execute('''SELECT id_carro, preco_venda FROM carro_venda''')
+        carro_vendas = cursor.fetchall()
+        for carro_venda in carro_vendas:
+            print(f"ID do carro: {carro_venda[0]}")
+            print(f"Preço da venda: {carro_venda[1]}")
+        
